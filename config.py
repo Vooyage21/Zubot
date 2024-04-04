6b729d9a63073c1d1c7ce79ad9077def2b015fde
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-04-05 01:21:30 +0700

    update config.py

--- config.py
+++ config.py
@@ -7,28 +7,28 @@ from dotenv import load_dotenv
 load_dotenv("config.env")
 
 ALIVE_EMOJI = getenv("ALIVE_EMOJI", "🥵")
-ALIVE_LOGO = getenv("ALIVE_LOGO", "https://graph.org/file/ec99cb6dba229bd984537.jpg")
+ALIVE_LOGO = getenv("ALIVE_LOGO", "https://mallucampaign.in/images/img_1708914936.jpg")
 ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey, I am alive.")
-API_HASH = getenv("API_HASH", "34efb38c74d5e6b25d1bb6234396a8af")
-API_ID = getenv("API_ID", "23129036")
+API_HASH = getenv("API_HASH", "7bc7d1f9b2f3d3f1bfd272db56ac0ba1")
+API_ID = getenv("API_ID", "26724473")
 BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
 BOT_VER = "3.0.0@main"
 BRANCH = getenv("BRANCH", "main") #don't change this line 
 CMD_HNDLR = cmd
-OWNER_ID = getenv("OWNER_ID", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "none")
+OWNER_ID = getenv("OWNER_ID", "940232666")
+BOT_TOKEN = getenv("BOT_TOKEN", "6674916212:AAFR7m4fB5Z98YxMh7pVNKf12v21rZBs4Y0")
 OPENAI_API_KEY = getenv("OPENAI_API_KEY", "")
-CHANNEL = getenv("CHANNEL", "Japanese_Userbot")
+CHANNEL = getenv("CHANNEL", "MusicStreamMp3")
 CMD_HANDLER = getenv("CMD_HANDLER", ".")
-DB_URL = getenv("DATABASE_URL", "")
+DB_URL = getenv("DATABASE_URL", "mongodb+srv://Xyxx:Helmi4636@cluster0.9yy5ve9.mongodb.net/?retryWrites=true&w=majority")
 GIT_TOKEN = getenv("GIT_TOKEN", "")
-GROUP = getenv("GROUP", "Japanese_Userbot_Support")
+GROUP = getenv("GROUP", "MusicStreamSupport")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
 PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
-REPO_URL = getenv("REPO_URL", "https://github.com/Team-Japanese/Japanese-X-Userbot")
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+REPO_URL = getenv("REPO_URL", "https://github.com/Vooyage21/Zubot.git")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQGXyHkAuq5VN9x9RfN7zt7wednQUB0PgBHtLZ4-OvC9RIah4QxTQfLNrwb0_kVcB_7Jhz5fbA6T18YUca52i5Y8LU-y0F_lOvkfukoKBZcSQOKP-aCdJlJKpITT-jzUMW8uqDDk5fX8NrlANSeY6MXOBtZpQ7DS0FZVaCdQJrxXrORuZQxSDuwvedUoex2wNcZPQxno6zSgAt8xy3mfn9Q-DraPxnplGLllYeKn3q7kd5_cE9JbtqqDcOe0u6qoUaijN_Ff85DK58zn7KZkE7KvysoainCbFktH4VdbL7edasxG3GTAcPeZ9i6k8sO1NWea7l1N9D-XA7SU3fktBDtQkOr8QQAAAAA4Cs_aAA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
@@ -38,8 +38,8 @@ STRING_SESSION7 = getenv("STRING_SESSION7", "")
 STRING_SESSION8 = getenv("STRING_SESSION8", "")
 STRING_SESSION9 = getenv("STRING_SESSION9", "")
 STRING_SESSION10 = getenv("STRING_SESSION10", "")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6694740726").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "940232666").split()))
 BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
 if not BLACKLIST_CHAT:
-    BLACKLIST_CHAT = [-1001608701614, -1001675459127, -1001473548283, -1001608701614]
-BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
+    BLACKLIST_CHAT = [-1001627039023]
+BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "-1001627039023").split()}
